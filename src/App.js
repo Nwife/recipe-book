a29db1f76ba91eff0582aee24bb6923ca253b3dc
Nwife Amaeze <ifeeamaeze@gmail.com>
2021-12-02 05:37:15 +0100

    created the page components and setup the react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+//page components
+import Home from './page/home/Home';
+import Create from './page/home/Home';
+import Search from './page/home/Home';
+import Recipe from './page/home/Home';
+
+//styles
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/recipes/:id">
+            <Recipe/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,2 +1,10 @@
 //styles
 import './Create.css';
+
+export default function Create() {
+    return (
+        <div>
+            Create
+        </div>
+    )
+}
