b30e9446f31b4222e88b2b29047f55cf30a76629
Nwife Amaeze <ifeeamaeze@gmail.com>
2021-12-03 11:42:34 +0100

    added the input setup for the ingredients

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import { useState } from 'react';
 
 //styles
@@ -7,10 +8,23 @@ export default function Create() {
     const [title, setTitle] = useState("");
     const [method, setMethod] = useState("");
     const [cookingTime, setCookingTime] = useState("");
+    const [newIngredient, setNewIngredient] = useState("");
+    const [ingredients, setIngredients] = useState([]);
+    const ingredientInput = useRef(null);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(title, method, cookingTime);
+        console.log(title, method, cookingTime, ingredients);
+    }
+
+    const handleAdd = (e) => {
+        e.preventDefault()
+        const ing = newIngredient.trim() //trim removes white space
+        if (ing && !ingredients.includes(ing)){
+            setIngredients(prevIngredients => [...prevIngredients, ing])
+        }
+        setNewIngredient("");
+        ingredientInput.current.focus();
     }
     
     return (
@@ -28,7 +42,19 @@ export default function Create() {
                     />
                 </label>
 
-                {/* Ingredients go here */}
+                <label>
+                    <span>Recipe ingredients:</span>
+                    <div className="ingredients">
+                        <input 
+                            type="text" 
+                            onChange= {(e) => setNewIngredient(e.target.value)}
+                            value= {newIngredient}
+                            ref= {ingredientInput}
+                        />
+                        <button onClick={handleAdd} className="btn">add</button>
+                    </div>
+                </label>
+                <p>Current ingredients: {ingredients.map(i => <em key={i}>{i},</em>)}</p>
 
                 <label>
                     <span>Recipe method:</span>
